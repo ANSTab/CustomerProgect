9780e56b7e82bd89f5fad3a4929d3b70915e779a
oleksii <oleksii.tabachenko@customertimes.com>
2023-10-31 18:25:16 +0200

    remove empty fields

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,12 +1,7 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class FirstTest {
